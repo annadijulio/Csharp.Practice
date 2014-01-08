add3b244ac7fd88d89a3e9f5b3cea18f5f6adb45
unknown <per.okvist@jayway.com>
2014-01-08 16:34:09 +0100

    Corrected mistake

--- Jayway.Csharp.Practice/Jayway.Csharp.Practice/Extensions/EnumerableExtensions.cs
+++ Jayway.Csharp.Practice/Jayway.Csharp.Practice/Extensions/EnumerableExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Jayway.Csharp.Practice
+﻿namespace Jayway.Csharp.Practice.Extensions
 {
     public static class EnumerableExtensions
     {
