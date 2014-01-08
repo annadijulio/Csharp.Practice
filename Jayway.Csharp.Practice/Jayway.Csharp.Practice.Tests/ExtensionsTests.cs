4ae5d1be9b6348018a5ecbe334602fe6e0044bd1
unknown <per.okvist@jayway.com>
2014-01-08 15:24:18 +0100

    Added Practice Projects

---
+++ Jayway.Csharp.Practice/Jayway.Csharp.Practice.Tests/ExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+using Jayway.Csharp.Practice.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jayway.Csharp.Practice.Tests
+{
+    [TestFixture]
+    public class ExtensionsTests
+    {
+
+        public void Times()
+        {
+            var result = 3.Times(3);
+
+            Assert.AreEqual(9, result);
+        }
+
+        public void IsNull()
+        {
+            //Arrange
+            object item = null;
+            
+            //Act
+            //var result = item.IsNull()
+
+            //Assert.IsTrue(result);
+        }
+
+        public void ForEach()
+        {
+            //Arrange
+            var ints = new ArrayList {1, 2, 3};
+            var i = 0;
+            //Act
+            //ints.ForEach(x => i=i+x);
+
+            //Assert
+            Assert.AreEqual(6, i);
+        }
+    }
+}
---
+++ Jayway.Csharp.Practice/Jayway.Csharp.Practice/Extensions/IntExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Jayway.Csharp.Practice.Extensions
+{
+    public static class IntExtensions
+    {
+        public static int Times(this int self, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
