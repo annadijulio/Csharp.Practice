b9664d12d3ab4ecc9f52775d78142f2c3a5b9f1e
annajayway <annadijulio@gmail.com>
2014-01-09 23:35:42 +0100

    car
    cartest

---
+++ Jayway.Csharp.Practice/Jayway.Cars.Infrastructure.Ef/CarsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jayway.Cars.Infrastructure.Ef
+{
+    public class CarContext : DbContext
+    {
+        public IDbSet<Domain.Car> Cars { get; set; }
+    }
+}
---
+++ Jayway.Csharp.Practice/Jayway.Csharp.Practice/Lab.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jayway.Csharp.Practice
+{
+    class Lab
+    {
+        public void test() {
+
+            var ORMlist = new List<Car> { new Car() { Id = 1}, new Car() { Id = 2} };
+           
+
+        }
+    }
+
+
+    public class InMemoryCarRepository : IRepository<T> {
+
+        IEnumerable<Car> IRepository<Car>.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IRepository<T> 
+        where T : int
+    {
+         IEnumerable<T> GetAll();
+    }
+}
